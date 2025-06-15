35ec33ac66f89d9ae5796d87ca6785650c687a8c
SAMAR KHALED <142172579+Samarmar2727@users.noreply.github.com>
2025-06-15 19:17:58 +0300

    add javascript task

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addButton = document.getElementById("addButton")
+    const taskInput = document.getElementById("task-input")
+    const taskList = document.getElementById("task-list")
+
+    function addTask() {
+        let taskText = taskInput.value.trim()
+
+        if(taskText){
+            const li = document.createElement("li");
+            li.textContent = taskText
+            const removeBtn = document.createElement("button");
+            removeBtn.textContent ="Remove"
+            removeBtn.className = "remove-btn"
+            removeBtn.onclick = () => li.remove();
+            li.appendChild(removeBtn); 
+             taskList.appendChild(li); 
+             taskInput.value = "";
+        }else{
+             alert("please add your task")
+        }
+
+    }
+
+     addButton.addEventListener("click", addTask);
+     taskInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            addTask();
+        }
+        });
+
+
+})
